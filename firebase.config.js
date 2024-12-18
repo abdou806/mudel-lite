9aaa9c606c8a510e77a5595519c15fcfe8246b88
abdelghani <bang10255@gmail.com>
2024-12-18 12:00:08 +0100

    Add files via upload

---
+++ firebase.config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDbMCpylKR7nx6ZcFfltgoP8ailVLYdufE",
+  authDomain: "model-lite.firebaseapp.com",
+  databaseURL: "https://model-lite-default-rtdb.firebaseio.com",
+  projectId: "model-lite",
+  storageBucket: "model-lite.firebasestorage.app",
+  messagingSenderId: "298093747408",
+  appId: "1:298093747408:web:2e4c3c70047e762d0692d5"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
